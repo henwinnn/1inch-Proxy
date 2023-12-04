dfcbfdad9fcf0f8652b818d4f90d69c80a78060a
Tanner Moore <tanner@1inch.io>
2023-12-04 11:30:36 -0700

    Readme added and POST is now supported

--- index.js
+++ index.js
@@ -5,23 +5,47 @@ const app = express();
 const port = 3000;
 
 const headers = {
-    "Authorization": process.env.AUTHORIZATION, // Read from .env
+    "Authorization": process.env.AUTHORIZATION,
     "Content-Type": "application/json"
 };
 
-app.get('/', async (req, res) => {
-    const url = req.query.url;
-    if (!url)
-        return res.status(400).send("include `url` in the query string");
-    if (!url.startsWith("https://api.1inch.dev"))
-        return res.status(400).send("base url must start with https://api.1inch.dev");
+// Middleware to handle request bodies
+app.use(express.json());
 
+// Middleware for URL validation
+app.use((req, res, next) => {
+    const url = req.query.url;
+
+    if (!url) {
+        return res.status(400).send("Include `url` in the query string or request body");
+    }
+    if (!url.startsWith("https://api.1inch.dev")) {
+        return res.status(400).send("Base URL must start with https://api.1inch.dev");
+    }
+    next();
+});
+
+app.get('/', async (req, res) => {
     try {
-        const response = await fetch(url, { headers });
+        const response = await fetch(req.query.url, { headers });
         const data = await response.json();
         return res.send(data);
     } catch (error) {
-        return res.status(500).send('Error occurred while fetching data');
+        return res.status(500).send('Error occurred while fetching data: ' + JSON.stringify(error));
+    }
+});
+
+app.post('/', async (req, res) => {
+    try {
+        const response = await fetch(req.query.url, {
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify(req.body.data)
+        });
+        const data = await response.json();
+        return res.send(data);
+    } catch (error) {
+        return res.status(500).send('Error occurred while fetching data: ' + JSON.stringify(error));
     }
 });
 
